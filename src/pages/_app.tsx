8a0d497b7c92f9727854e26e15f199d29c07219a
Matěj Pešl <matejpesl1@gmail.com>
2024-03-10 16:13:37 +0100

    feat: setup chakra and supabase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,49 @@
-import { type AppType } from "next/app";
+import type { AppType } from "next/app";
 import { Inter } from "next/font/google";
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+// Later we can use this to decrease JS size.
+// const theme = extendBaseTheme({
+//   components: {
+//     Button,
+//   },
+// })
+
+// <ChakraBaseProvider theme={theme}>
+
+export const theme = extendTheme({
+  fonts: {
+    heading: "var(--font-inter)",
+    body: "var(--font-inter)",
+  },
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={`font-sans ${inter.variable}`}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      {/* Do this to be able to import the font in chakra. */}
+      <style jsx global>
+        {`
+            :root {
+                --font-inter: ${inter.style.fontFamily};
+            }
+        `}
+      </style>
+      <ChakraProvider theme={theme}>
+        <main className={`font-sans ${inter.variable}`}>
+          <Component {...pageProps} />
+        </main>
+      </ChakraProvider>
+    </>
   );
 };
 
